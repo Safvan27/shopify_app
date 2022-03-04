debe37b73e6509d1b58627b044a1cbabd4d6da94
unknown <safvanparangodath12@gmail.com>
2022-03-04 19:17:23 +0530

    first commit

---
+++ ci-cd/infrastructure/lib/config.ts
@@ -0,0 +1,11 @@
+export const envSecretsConfig = {
+  secretKeys: {
+    shopifyApiKey: "SHOPIFY_API_KEY",
+    shopifyApiSecret: "SHOPIFY_API_SECRET",
+    shop: "SHOP",
+    scopes: "SCOPES",
+    host: "HOST",
+  },
+};
+
+export const domain = "domain.com";
